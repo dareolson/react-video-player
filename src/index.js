8a5e13e2a6c06956e6083f13e62aeed855b86383
Derek <dare.olson@gmail.com>
2017-10-26 15:26:35 -0700

    adding youtube API key and youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyBXmIKC6o-cfMgocfCDeZQ-fLo07WzSgHw';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+	const App = () => {
+		return <div>Fuckit Boi </div>
+	}
+
+ReactDOM.render(<App />, document.querySelector('.container' ));
