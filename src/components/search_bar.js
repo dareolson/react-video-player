39a1449a3fcbd0de8deea3b658883665adc09a7f
Derek <dare.olson@gmail.com>
2017-10-27 15:51:09 -0700

    state update in search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,24 @@ import React, {Component } from 'react';
 
 // const Component = React.Component;
 
-class SearchBar extends React.Component {
-	render(){
-		return <input onChange = {event => console.log(event.target.value)} />
+class SearchBar extends Component {
 
+	constructor(props){
+		super(props);
+
+		this.state = {term: ''};
 	}
 
+	render(){
+		return (
+		<div>
+		<input 
+			value = {this.state.term}
+			onChange = {event => this.setState({ term: event.target.value})} />
 
+		</div>
+		);
+	}
 }
 
 export default SearchBar;
