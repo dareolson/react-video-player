19cc925bc7e35855a9254575695e8493ea172a27
Derek <dare.olson@gmail.com>
2017-10-30 13:57:43 -0700

    rendering video search results in dom with jsx

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBXmIKC6o-cfMgocfCDeZQ-fLo07WzSgHw';
 
 
@@ -10,17 +11,24 @@ const API_KEY = 'AIzaSyBXmIKC6o-cfMgocfCDeZQ-fLo07WzSgHw';
 		constructor(props){
 			super(props);
 
-			this.state = { videos: [] };
+			this.state = {
+			videos: [],
+			selectedVideo: null
+			};
 
 			YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({
+			videos: videos,
+			selectedVideo: videos [0]
 			});
-		}
+		});
+	}
 
 		render() {
 		return (
 		<div> 
 			<SearchBar />
+			<VideoDetail video = {this.state.selectedVideo} />
 			<VideoList videos = {this.state.videos}/>
 		</div>
 		)
